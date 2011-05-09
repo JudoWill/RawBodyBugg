9fb292c38fbb3b1343ed4efaca5e8dac847504f2
Will Dampier <judowill@gmail.com>
2011-05-09 15:47:28 -0400

    added test for the appending calls

--- testBasic.py
+++ testBasic.py
@@ -33,6 +33,35 @@ def test_basic_call():
     assert os.path.exists('nfile.csv'), 'Did not create file!'
 
     check_equal_files('nfile.csv', 'ftest.csv')
+
+def test_appending_call():
+
+    if os.path.exists('nfile.csv'):
+        os.remove('nfile.csv')
+    
+    if os.path.exists('nfile.cpickle'):
+        os.remove('nfile.cpickle')
+    
+    cmd = shlex.split('python bmhack.py --fromDump=ftest.cpickle --toCsv=nfile.csv --toDump nfile.cpickle')
+    check_call(cmd)
+
+    cmd = shlex.split('python bmhack.py --fromDump=ftest.cpickle --toCsv=nfile.csv --toDump nfile.cpickle')
+    check_call(cmd)
+
+    test_rows = list(csv.DictReader(open('nfile.csv'), delimiter = '\t'))
+    cor_rows = list(csv.DictReader(open('ftest.csv'), delimiter = '\t'))
+    cor_rows = cor_rows+cor_rows
+    assert len(cor_rows) == len(test_rows), 'Files are not the same length! (%i, %i)' % (len(test_rows), len(cor_rows))
+    for ind, (cor, tes) in enumerate(izip_longest(cor_rows, test_rows)):
+        tes.pop(None, None)
+        cor.pop(None, None)
+        assert cor == tes, str(cor) + str(tes) + str(ind)
+    
+    cor_rows = cPickle.load(open('ftest.cpickle')) + cPickle.load(open('ftest.cpickle'))
+    test_rows = cPickle.load(open('nfile.cpickle'))
+    assert cor_rows == test_rows, 'Pickle files unequal!'
+
+
     
 def test_main_call_csv_only():
     
